982d33ce7078061681be2fde7f159337ec2d805c
Mdante-033 <danielmunyiri97@gmail.com>
2026-06-05 14:09:24 +0300

    Add session_start() at the top and enable error logging for better debugging

--- luxestore.php
+++ luxestore.php
@@ -1,4 +1,9 @@
-﻿<?php
+<?php
+session_start();
+error_reporting(E_ALL);
+ini_set('display_errors', 0);
+ini_set('log_errors', 1);
+ini_set('error_log', __DIR__ . '/logs/php_errors.log');
 
 $year = date('Y');
 $contact_name = "James";
